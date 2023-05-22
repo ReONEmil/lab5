f087224e8c0df0c8dde38c2d8199704bf3adfa06
ReONEmil <alizadaemil49@gmail.com>
2023-05-22 16:21:55 +0400

    Lab-5

---
+++ scirpt.js
@@ -0,0 +1,23 @@
+function appendToDisplay(value) {
+    var display = document.getElementById('display');
+    display.innerHTML += value;
+  }
+  
+  function clearDisplay() {
+    var display = document.getElementById('display');
+    display.innerHTML = '';
+  }
+  
+  function deleteLastCharacter() {
+    var display = document.getElementById('display');
+    var currentValue = display.innerHTML;
+    display.innerHTML = currentValue.slice(0, -1);
+  }
+  
+  function calculateResult() {
+    var display = document.getElementById('display');
+    var expression = display.innerHTML;
+    var result = eval(expression); // Note: eval() is used here for simplicity, but it's generally not recommended for security reasons.
+    display.innerHTML = result;
+  }
+  
